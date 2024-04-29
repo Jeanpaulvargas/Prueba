089da6e7c7acec536a112cdd489120f7b0bfc7b9
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-04-29 09:08:59 -0600

    subir cambios

--- prueba2.php
+++ prueba2.php
@@ -1,1 +1,13 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Programacion orientada a objetos</h1>
+    <h2>inicio y clonacion</h2>
+</body>
+</html>
